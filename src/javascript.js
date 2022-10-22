c8c414bdc4994202816000cd13c282094f82f04a
softoro <112343085+softoro@users.noreply.github.com>
2022-10-22 20:49:37 +0200

    Added the search engine

--- src/javascript.js
+++ src/javascript.js
@@ -40,7 +40,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "0ba26cb539d3aaf2d4044of4b6t30f1f";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "0ba26cb539d3aaf2d4044of4b6t30f1f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
